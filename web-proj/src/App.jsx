4d1219fcfe1e64252e50543bee2c57b89804dfdd
Jayem09 <johndinglasan12@gmail.com>
2025-06-23 11:07:07 +0800

    idk

--- web-proj/src/App.jsx
+++ web-proj/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import LandingPage from "./dashboard/landingPage";
 import AtmosphericPropertiesCalculator from './components/AtmosphericPropertiesCalculator';
-import Quizsystem from './components/module1/QuizSystem';
+import QuizSystem from './components/module1/QuizSystem';
 
 function App() {
   return (
     <>
       <LandingPage />
       <AtmosphericPropertiesCalculator />
-      <Quizsystem />
+      <QuizSystem />
     </>
   );
 }

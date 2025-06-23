305dd9c149e7392645437fd5dfaa3d6dfbd39d7e
Jayem09 <johndinglasan12@gmail.com>
2025-06-23 11:29:34 +0800

    adjust chunk

--- web-proj/vite.config.js
+++ web-proj/vite.config.js
@@ -9,4 +9,16 @@ export default defineConfig({
       overlay: false,
     },
   },
+  build: {
+    chunkSizeWarningLimit: 2000, // Set chunk size warning limit to 2000 KB (2MB)
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split your largest dependencies into separate chunks
+          react: ['react', 'react-dom'],
+          vendor: ['lodash', 'axios'], // Add other large dependencies here
+        },
+      },
+    }
+  }
 })
